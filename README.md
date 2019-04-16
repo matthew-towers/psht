92c342af9c9c8cca0460332d7b98e8190457278d
Matthew Towers <matthew.towers@gmail.com>
2019-04-16 20:24:25 +0100

    allow multiple audiences

--- README.md
+++ README.md
@@ -1,21 +1,32 @@
 # psht
 
-This bash script compiles a problem sheet written in LaTeX into two pdf
-files, one including solutions and one not including solutions.
+This bash script compiles a LaTeX document written using the
+`multiaudience' package into multiple pdf files, one for each audience
+specified.
 
 To make it work you must have the
 [`multiaudience`](https://ctan.org/pkg/multiaudience?lang=en) package
 installed on your system, as well as
-[`latexmk`](http://personal.psu.edu/jcc8//software/latexmk-jcc/).  Put `\usepackage{multiaudience}`,
+[`latexmk`](http://personal.psu.edu/jcc8//software/latexmk-jcc/).  You
+need `\usepackage{multiaudience}` in the preamble.
+
+If you run `./psht your-tex-file.tex`, psht will assume that there are
+two audiences defined, students and tutors.  This means you must have 
 `\SetNewAudience{students}`, and `\SetNewAudience{tutors}` in the
-preamble.
+preamble.  It will create `your-tex-file.pdf` and
+`your-tex-file_SOLUTIONS.pdf`. Compilation is done using `latexmk` and
+the auxiliary files are removed afterwards.
 
 Surround the parts of the LaTeX document you want to appear ONLY in the
 solutions file with `\begin{shownto}{-, students} ...
 \end{shownto}`. The `-` is negation.
 
-Then run `./psht your-tex-file.tex`. This will create
-`your-tex-file.pdf` and `your-tex-file_SOLUTIONS.pdf`. Compilation is
-done using `latexmk` and the auxiliary files are removed afterwards.
-
 A test file called `test.tex` is included as illustration.
+
+If you supply more arguments after the filename, e.g. `./psht
+your-tex-file.tex a b c` psht will compile a version of the document for
+each additional argument `arg`, with the audience set to `arg`.  The
+filenames will be `your-tex-file_arg.pdf`.
+
+A test file called `multiple_audiences.tex` is included, which has three
+audiences `a`, `b`, and `c`.
--- psht.sh
+++ psht.sh
@@ -6,27 +6,49 @@ filenameWithExtension=$1
 extension="${filenameWithExtension##*.}"
 filename="${filenameWithExtension%.*}"
 
-if [ -e "${filename}_SOLUTIONS.tex" ]
+if [ $# -eq 1 ]  # one filename arg, so assume students and tutors
 then
-    echo "Solutions tex file already exists. Quitting."
-    exit 1
+    if [ -e "${filename}_SOLUTIONS.tex" ]
+    then
+        echo "Solutions tex file already exists. Quitting."
+        exit 1
+    fi
+
+    if [ -e "${filename}_TEMP.tex" ]
+    then
+        echo "Temp tex file already exists. Quitting."
+        exit 1
+    fi
+
+
+    (echo -e '\\def\\CurrentAudience{tutors}'; cat "$filenameWithExtension") > "${filename}_SOLUTIONS.tex"
+
+    (echo -e '\\def\\CurrentAudience{students}'; cat "$filenameWithExtension") > "${filename}_TEMP.tex"
+
+    latexmk -pdf "${filename}_SOLUTIONS.tex"
+    latexmk -c
+    latexmk -pdf -jobname="$filename" "${filename}_TEMP.tex"
+    latexmk -c
+
+    rm "${filename}_TEMP.tex" 
+    rm "${filename}_SOLUTIONS.tex"
+else
+    # further arguments were supplied, later ones are audiences
+    # for each argument AUD after the first,
+    #  - check if filename_AUD.tex exists. If so, quit
+    #  - otherwise make filename_AUD.tex with the appropriate prefix
+    #  - then compile it and clean up
+    #  - then erase the temporary file filename_AUD.tex
+    for audience in "${@:2}" 
+    do
+        if [ -e "${filename}_$audience.tex" ]
+        then
+            echo "${filename}_$audience.tex already exists. Quitting."
+            exit 1
+        fi
+        (echo -e "\\def\\CurrentAudience{${audience}}"; cat "$filenameWithExtension") > "${filename}_${audience}.tex"
+        latexmk -pdf "${filename}_${audience}.tex"
+        latexmk -c
+        rm "${filename}_${audience}.tex"
+    done
 fi
-
-if [ -e "${filename}_TEMP.tex" ]
-then
-    echo "Temp tex file already exists. Quitting."
-    exit 1
-fi
-
-
-(echo -e '\\def\\CurrentAudience{tutors}'; cat "$filenameWithExtension") > "${filename}_SOLUTIONS.tex"
-
-(echo -e '\\def\\CurrentAudience{students}'; cat "$filenameWithExtension") > "${filename}_TEMP.tex"
-
-latexmk -pdf "${filename}_SOLUTIONS.tex"
-latexmk -c
-latexmk -pdf -jobname="$filename" "${filename}_TEMP.tex"
-latexmk -c
-
-rm "${filename}_TEMP.tex" 
-rm "${filename}_SOLUTIONS.tex"
